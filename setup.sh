cb756a5dec31be0849488722f9a3261a4b932210
chikhidev <chikhi.dev@gmail.com>
2024-11-03 11:14:38 +0100

    this is my default commit

--- setup.sh
+++ setup.sh
@@ -8,9 +8,12 @@ done
 
 if [ -f ~/.zshrc ]; then
     echo 'alias gitsync="python3 ~/githubSync/index.py"' >> ~/.zshrc
+    echo 'pgrep -f "gitsync run:scheduler" > /dev/null || gitsync run:scheduler &' >> ~/.zshrc
     zsh
+
 elif [ -f ~/.bashrc ]; then
     echo 'alias gitsync="python3 ~/githubSync/index.py"' >> ~/.bashrc
+    echo 'pgrep -f "gitsync run:scheduler" > /dev/null || gitsync run:scheduler &' >> ~/.bashrc
     source ~/.bashrc
 else
     echo "No .zshrc or .bashrc file found"
